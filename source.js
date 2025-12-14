995993d97de58cac6518e4b43b79f49e4e32cf03
AndriiTaizhkorob <andriitiazhkorob007@gmail.com>
2025-12-14 13:42:27 +0100

    Portfolio

---
+++ source.js
@@ -0,0 +1,59 @@
+const swiper0 = new Swiper("#portfolioSwiper", {
+    loop: true,
+    speed: 800,
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false
+    },
+    effect: "slide", // try "fade" for a subtler vibe
+    pagination: {
+        el: "#portfolioSwiper .swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: "#portfolioSwiper .swiper-button-next",
+        prevEl: "#portfolioSwiper .swiper-button-prev"
+    },
+    keyboard: { enabled: true },
+    a11y: { enabled: true }
+});
+
+const swiper1 = new Swiper("#portfolioSwiper1", {
+    loop: true,
+    speed: 800,
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false
+    },
+    effect: "slide", // try "fade" for a subtler vibe
+    pagination: {
+        el: "#portfolioSwiper1 .swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: "#portfolioSwiper1 .swiper-button-next",
+        prevEl: "#portfolioSwiper1 .swiper-button-prev"
+    },
+    keyboard: { enabled: true },
+    a11y: { enabled: true }
+});
+
+const swiper2 = new Swiper("#portfolioSwiper2", {
+    loop: true,
+    speed: 800,
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false
+    },
+    effect: "slide", // try "fade" for a subtler vibe
+    pagination: {
+        el: "#portfolioSwiper2 .swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: "#portfolioSwiper2 .swiper-button-next",
+        prevEl: "#portfolioSwiper2 .swiper-button-prev"
+    },
+    keyboard: { enabled: true },
+    a11y: { enabled: true }
+});
